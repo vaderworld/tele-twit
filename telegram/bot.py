c15d88505de7fdf023b192fbd9fd2a24dfe8f44b
Evgeny Nosovitsky <vaderworld2009@gmail.com>
2016-12-08 12:38:36 +0300

    Twitter bot is class now

--- telegram/bot.py
+++ telegram/bot.py
@@ -1,5 +1,7 @@
 import telebot
 
+from twitter.bot import TwitterInteractor
+
 from telegram import config
 
 bot = telebot.TeleBot(config.token)
@@ -7,12 +9,15 @@ bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(commands=['/send'])
 def send_to_twitter(message):
-    pass
+    twitter.send_message(message)
 
 
 @bot.message_handler(commands=['/get'])
 def get_from_twitter(amount):
-    pass
+    print('received get request')
+    tweets = twitter.get_messages(amount)
+    bot.send_message(amount.chat.id, tweets)
 
 if __name__ == '__main__':
+    twitter = TwitterInteractor()
     bot.polling(none_stop=True)
--- twitter/bot.py
+++ twitter/bot.py
@@ -2,10 +2,20 @@ import tweepy
 
 from twitter import config
 
-auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
-auth.set_access_token(config.access_token, config.access_secret)
 
-api = tweepy.API(auth)
+class TwitterInteractor:
+    def __init__(self):
+        self.auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
+        self.auth.set_access_token(config.access_token, config.access_secret)
+        self.api = tweepy.API(self.auth)
 
-for status in tweepy.Cursor(api.friends).items():
-    print(status)
+    def get_messages(self, amount):
+        public_tweets = self.api.home_timeline(count=amount)
+        return list(map(self.format_tweet, public_tweets))
+
+    def send_message(self, message):
+        self.api.update_status(message)
+
+    @staticmethod
+    def format_tweet(tweet):
+        return '{} at {} ---- {}'.format(tweet.author.name, tweet.created_at, tweet.text)
