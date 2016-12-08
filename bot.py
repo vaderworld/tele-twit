86f3cb67bded7dc2e5856a0712d9b284bcaf5a84
Evgeny Nosovitsky <vaderworld2009@gmail.com>
2016-12-08 14:59:32 +0300

    Added start and help

--- bot.py
+++ bot.py
@@ -5,6 +5,10 @@ from twitter.TwitterInteractor import TwitterInteractor
 bot = telebot.TeleBot(config.token)
 
 
+@bot.message_handler(commands=['start', 'help'])
+def show_help(message):
+    bot.send_message(message.chat.id, 'Commands:\n/send <msg> - send <msg> to twitter\n/get <n> - get <n> last tweets from timeline')
+
 @bot.message_handler(commands=['send'])
 def send_to_twitter(message):
     tweet_msg = message.text[len('/send '):]
